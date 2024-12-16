6bb2d37aad06490e5c565cf7eb367aab7a2f466b
Adrian Galsim <adriangalsim@adrians-mbp.mynetworksettings.com>
2024-12-16 07:22:56 -0500

    final submission

---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+Submitted by: Adrian Galsim (directory id: agalsim)
+Group Members: Adrian Galsim (agalsim), Sophia Gonzales (sgonza13)
+App Description: Dating Website Tailored For Computer Science Majors
+Youtube Link: 
+API: Nerdy PickUp Lines (https://rapidapi.com/roomasiddiqui2003/api/nerdy-pickup-lines1/playground/apiendpoint_6a83bea3-9433-4a30-b3cd-b1f68aff006d)
+Email: agalsim@terpmail.umd.edu
--- finalProjServer.js
+++ finalProjServer.js
@@ -1,6 +1,3 @@
-//below contains the set up to mongodb and other libraries
-//I used what I had in project 5, however besides set up this project is not an extension
-
 const path = require("path");
 const bodyParser = require("body-parser");
 require("dotenv").config({ path: path.resolve(__dirname, 'credentials/.env') });
@@ -16,14 +13,30 @@ const fs = require("fs");
 process.stdin.setEncoding("utf8");
 const port = process.argv[2];
 let count = 0;
+app.use('/css', express.static(path.join(__dirname, 'css')));
+app.use('/image', express.static(path.join(__dirname, 'image')));
 app.set("views", path.resolve(__dirname, "templates"));
 app.set("view engine", "ejs");
 
-let curuser = "N/A";
+//API using fetch from rapidAPI
+const url = 'https://nerdy-pickup-lines1.p.rapidapi.com/pickup_lines';
+const options = {
+	method: 'GET',
+	headers: {
+		'x-rapidapi-key': 'a565de5e7bmsh79627f7261eadf1p15e847jsn522bed93c0f2',
+		'x-rapidapi-host': 'nerdy-pickup-lines1.p.rapidapi.com'
+	}
+};
 
 app.get("/", (request, response) => {
-    response.render("login");
+  const error = "";
+    response.render("login", {error});
   });
+
+  app.get("/logIn", (request, response) => {
+    const error = "";
+      response.render("login", {error});
+    });
 
 app.get("/signUp", (request, response) => {
       response.render("signUp");
@@ -35,21 +48,77 @@ app.post("/welcome", async (request, response) => {
     response.render("welcome", {user, email, password, password2});
 });
 
-app.get("/account", async (request, response) => {
-  let name, email, gender, age, preference, language, university, description = curuser, curemail, curgender, curage, curpreference, curlanguage, curuniversity, curdescription;
-  response.render("account", {name, email, gender, age, preference, language, university, description});
-});
-
 app.post("/account", async (request, response) => {
   let {name, gender, age, preference, language, university, description} = request.body;
   const answer = await editAccount(name, gender, age, preference, language, university, description);
-  const email = answer.email;
-  response.render("account", {name, email, gender, age, preference, language, university, description});
+  if (!answer){
+    response.render("edit");
+  } else {
+    const email = answer.email;
+    response.render("account", {name, email, gender, age, preference, language, university, description});
+  }
+
 });
+
+app.post("/accountLogIn", async (request, response) => {
+  let {user, password} = request.body;
+  const answer = await accessInfo(user);
+  if (!answer || password != answer.password){
+    const error = "Wrong Credentials";
+    response.render("login", {error});
+  } else {
+    const name = user;
+    const email = answer.email;
+    const gender = answer.gender;
+    const age = answer.age;
+    const preference = answer.preference;
+    const language = answer.language;
+    const university = answer.university;
+    const description = answer.description;
+    response.render("account", {name, email, gender, age, preference, language, university, description});
+  }
+
+});
+
+
 
 app.get("/edit", (request, response) => {
   response.render("edit");
 });
+
+app.get("/match", async (request, response) => {
+  const answer = await randomUser();
+  const name = answer.user;
+  const email = answer.email;
+  const gender = answer.gender;
+  const age = answer.age;
+  const preference = answer.preference;
+  const language = answer.language;
+  const university = answer.university;
+  const description = answer.description;
+  response.render("match", {name, gender, age, preference, language, university, description});
+})
+
+app.post("/matched", async (request, response) => {
+  let {matched} = request.body;
+  let access = await accessInfo(matched);
+  if (!access){
+    const answer = await randomUser();
+    const name = answer.user;
+    const gender = answer.gender;
+    const age = answer.age;
+    const preference = answer.preference;
+    const language = answer.language;
+    const university = answer.university;
+    const description = answer.description;
+    response.render("match", {name, gender, age, preference, language, university, description});
+  } else {
+    const name = access.user;
+    const email = access.email;
+    const pickup = await getJoke();
+    response.render("matched", {name, email, pickup});
+  }
+})
 
   //functions for mongodb
   async function addAccount(user, email, password){
@@ -93,7 +162,33 @@ async function accessInfo(user){
     await client.close();
   }
 }
+async function randomUser(){
+  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+  try {
+    await client.connect();
+    const result = await client.db(databaseCollection.db).collection(databaseCollection.collection).aggregate([{$sample: {size: 1}}]).next();
+    return result
+  } catch (e) {
+    console.error(e);
+  } finally {
+    await client.close();
+  }
+}
 
+async function getJoke(){
+  try {
+    const response = await fetch(url, options);
+    const result = await response.json();
+    console.log(result);
+    let linesList = result.nerdy_pickup_lines;
+    const randomNum = Math.floor(Math.random()*linesList.length);
+    let line = linesList[randomNum];
+    return line;
+
+  } catch (error) {
+    console.error(error);
+  }
+}
 
   //use node finalProjServer.js (any number) to access
 
